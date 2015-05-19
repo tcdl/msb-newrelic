aa6c63c1240d14b06e467a375d51f97feb1d9018
jupiter <pieter@wavana.com>
2015-05-19 17:08:02 +0100

    Added uncaughtException handling

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var msb = require('msb');
-msb.plugins = msb.plugins || {};
 
-if (!process.env.NEW_RELIC_HOME) process.env.NEW_RELIC_HOME = __dirname;
-var newrelic = msb.plugins.newrelic = require('newrelic');
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+  msb.plugins = msb.plugins || {};
 
-msb.plugins.http2busMiddleware = function(route) {
-  return function(req, res, next) {
-    newrelic.setTransactionName(route.http.path);
-    next();
+  if (!process.env.NEW_RELIC_HOME) process.env.NEW_RELIC_HOME = __dirname;
+  var newrelic = msb.plugins.newrelic = msb.plugins.newrelic || require('newrelic');
+
+  msb.plugins.http2busMiddleware = function(route) {
+    return function(req, res, next) {
+      newrelic.setTransactionName(route.http.path);
+      next();
+    };
   };
-};
+
+  process.once('uncaughtException', function(err) {
+    newrelic.addCustomParameter('crash', 'true');
+    newrelic.noticeError(err);
+    newrelic.agent.harvest(function() {
+      setTimeout(function() {
+        process.exit(1);
+      }, 1000);
+    });
+  });
+}
 
 module.exports = msb;
---
+++ test.js
@@ -0,0 +1,33 @@
+// Note: you will need to have installed `msb` manually to run this test
+var assert = require('assert');
+var simple = require('simple-mock');
+var msb = require('msb');
+
+msb.plugins = msb.plugins || {};
+var mockNewrelic = msb.plugins.newrelic = {
+  agent: {}
+};
+
+simple.mock(mockNewrelic, 'addCustomParameter').returnWith();
+simple.mock(mockNewrelic, 'noticeError').returnWith();
+simple.mock(mockNewrelic.agent, 'harvest').callbackWith();
+simple.mock(process, 'exit').returnWith();
+simple.mock(process.env, 'NEW_RELIC_LICENSE_KEY', 'something');
+
+var msb = require('./index');
+var error = new Error('my uncaught exception');
+
+setTimeout(function() {
+  try {
+    assert(mockNewrelic.addCustomParameter.called);
+    assert(mockNewrelic.noticeError.called);
+    assert.equal(mockNewrelic.noticeError.lastCall.arg, error);
+    assert(process.exit.called);
+  } catch (e) {
+    return console.log(e.message, 'on line ' + e.stack.match(/\d+/)[0]);
+  }
+  console.log('passed');
+}, 2000);
+
+// Test code
+throw error;
